79797867a018b53b9c1cf9ba7850cf4c60b2ba2f
3dw1nM0535 <workockmoses@gmail.com>
2023-10-14 23:54:38 +0300

    more ui updates

--- app/src/main/java/com/example/nyatta/ui/screens/listing/Amenities.kt
+++ app/src/main/java/com/example/nyatta/ui/screens/listing/Amenities.kt
@@ -1,14 +1,23 @@
 package com.example.nyatta.ui.screens.listing
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nyatta.ui.theme.NyattaTheme
 
 @Composable
 fun ListingAmenity(modifier: Modifier = Modifier) {
-    Text("Amenities")
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Amenities")
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/nyatta/ui/screens/listing/Photos.kt
+++ app/src/main/java/com/example/nyatta/ui/screens/listing/Photos.kt
@@ -1,14 +1,22 @@
 package com.example.nyatta.ui.screens.listing
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nyatta.ui.theme.NyattaTheme
 
 @Composable
 fun ListingPhoto(modifier: Modifier = Modifier) {
-    Text("Photos")
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Photos")
+    }
 }
 
 @Preview(showBackground = true)
