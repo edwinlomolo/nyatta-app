248cc591b37d9e6a2e6e4f6986d506690274f2d0
3dw1nM0535 <workockmoses@gmail.com>
2023-10-16 19:39:31 +0300

    chore: listing photos

--- app/src/main/java/com/example/nyatta/ui/screens/listing/Photos.kt
+++ app/src/main/java/com/example/nyatta/ui/screens/listing/Photos.kt
@@ -1,12 +1,21 @@
 package com.example.nyatta.ui.screens.listing
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.nyatta.R
 import com.example.nyatta.ui.theme.NyattaTheme
 
 @Composable
@@ -15,7 +24,22 @@ fun ListingPhoto(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Photos")
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Adaptive(200.dp),
+            verticalItemSpacing = 4.dp,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            content = {
+                items(10) {
+                    Image(
+                        painterResource(R.drawable.rottweiler),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxWidth().wrapContentHeight()
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
