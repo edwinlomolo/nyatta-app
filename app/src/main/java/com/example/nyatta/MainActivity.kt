6c09e1a93d39d0757e775e959004a6537db1e458
3dw1nM0535 <workockmoses@gmail.com>
2023-10-24 18:14:23 +0300

    feat(gps): get device location on start

--- app/src/main/java/com/example/nyatta/MainActivity.kt
+++ app/src/main/java/com/example/nyatta/MainActivity.kt
@@ -1,32 +1,176 @@
 package com.example.nyatta
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.example.nyatta.ui.theme.NyattaTheme
 import com.example.nyatta.ui.NyattaApp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var hasLocationPermissions by remember { mutableStateOf(hasLocationPermissions())}
+            var shouldShowPermissionRationale by remember {
+                mutableStateOf(
+                    shouldShowRequestPermissionRationale(
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
+            }
+            var currentPermissionStatus by remember {
+                mutableStateOf(
+                    decideLocationPermissionStatus(
+                        hasLocationPermissions,
+                        shouldShowPermissionRationale
+                    )
+                )
+            }
+            val locationPermissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            var shouldDirectUserToApplicationSettings by remember {
+                mutableStateOf(false)
+            }
+            val locationPermissionLauncher =
+                rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestMultiplePermissions()
+                ) { permissions ->
+                    hasLocationPermissions = permissions.values.reduce { acc, isPermissionGranted ->
+                        acc && isPermissionGranted
+                    }
+
+                    if (!hasLocationPermissions) {
+                        shouldShowPermissionRationale =
+                            shouldShowRequestPermissionRationale(
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            )
+                    }
+
+                    shouldDirectUserToApplicationSettings = !shouldShowPermissionRationale && !hasLocationPermissions
+                    currentPermissionStatus = decideLocationPermissionStatus(
+                        hasLocationPermissions,
+                        shouldShowPermissionRationale
+                    )
+                }
+            val lifecycleOwner = LocalLifecycleOwner.current
+            DisposableEffect(
+                key1 = lifecycleOwner,
+                effect = {
+                    val observer = LifecycleEventObserver { _, event, ->
+                        if (event == Lifecycle.Event.ON_START &&
+                            !hasLocationPermissions &&
+                            !shouldShowPermissionRationale) {
+                            locationPermissionLauncher.launch(locationPermissions)
+                        }
+                    }
+                    lifecycleOwner.lifecycle.addObserver(observer)
+                    onDispose {
+                        lifecycleOwner.lifecycle.removeObserver(observer)
+                    }
+                }
+            )
+
+            val scope = rememberCoroutineScope()
+            val snackbarHostState = remember { SnackbarHostState() }
+
             NyattaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    NyattaApp()
+                Scaffold(
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+                ) { innerPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        if (shouldShowPermissionRationale) {
+                            LaunchedEffect(Unit) {
+                                scope.launch {
+                                    val userAction = snackbarHostState.showSnackbar(
+                                        message = "Authorize location permission",
+                                        actionLabel = "Approve",
+                                        duration = SnackbarDuration.Indefinite,
+                                        withDismissAction = true
+                                    )
+                                    when (userAction) {
+                                        SnackbarResult.ActionPerformed -> {
+                                            shouldShowPermissionRationale = false
+                                            locationPermissionLauncher.launch(locationPermissions)
+                                        }
+                                        SnackbarResult.Dismissed -> {
+                                            shouldShowPermissionRationale = false
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (shouldDirectUserToApplicationSettings) {
+                            openApplicationSettings()
+                        }
+                        NyattaApp()
+                    }
                 }
             }
         }
+    }
+
+    private fun openApplicationSettings() {
+        Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", "com.example.nyatta", null)).also {
+            startActivity(it)
+        }
+    }
+    private fun hasLocationPermissions(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun decideLocationPermissionStatus(
+        hasLocationPermission: Boolean,
+        shouldShowPermissionRationale: Boolean
+    ): String {
+        return if (hasLocationPermission) "Granted"
+        else if (shouldShowPermissionRationale) "Rejected"
+        else "Denied"
     }
 }
 
