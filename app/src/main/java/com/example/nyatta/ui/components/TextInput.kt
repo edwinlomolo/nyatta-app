e91c6ed23375b4c7c143d579a61b0a246b064297
3dw1nM0535 <workockmoses@gmail.com>
2023-10-28 11:24:38 +0300

    fix: cleanup

--- app/src/main/java/com/example/nyatta/ui/components/TextInput.kt
+++ app/src/main/java/com/example/nyatta/ui/components/TextInput.kt
@@ -1,17 +1,14 @@
 package com.example.nyatta.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun TextInput(
