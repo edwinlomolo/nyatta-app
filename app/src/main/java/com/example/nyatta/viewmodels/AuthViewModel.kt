bc30675880a059bb6cff237c77c39b10a2a2c948
3dw1nM0535 <workockmoses@gmail.com>
2023-11-10 02:49:38 +0300

    fix: api calls repeat onInit

--- app/src/main/java/com/example/nyatta/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/example/nyatta/viewmodels/AuthViewModel.kt
@@ -3,7 +3,6 @@ package com.example.nyatta.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.nyatta.data.auth.OfflineAuthRepository
-import com.example.nyatta.data.model.User
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -25,24 +24,12 @@ class AuthViewModel(
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
             ).collect { user ->
                 if (user.isNotEmpty()) {
-                    val response = authRepository.signUp(user[0].phone)
-                    if (response.hasErrors()) {
-                        _auth.update {
-                            it.copy(
-                                token = user[0].token,
-                                phone = user[0].phone,
-                                isAuthed = user[0].token.isNotEmpty(),
-                                isLandlord = user[0].isLandlord
-                            )
-                        }
-                    } else {
-                        val res = response.data?.signIn
-                        authRepository.signUser(
-                            user = User(
-                                token = res!!.Token,
-                                phone = res.user.phone,
-                                isLandlord = res.user.is_landlord
-                            )
+                    _auth.update {
+                        it.copy(
+                            token = user[0].token,
+                            phone = user[0].phone,
+                            isAuthed = user[0].token.isNotEmpty(),
+                            isLandlord = user[0].isLandlord
                         )
                     }
                 }
